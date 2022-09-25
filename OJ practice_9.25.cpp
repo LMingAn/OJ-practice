80faadb4058adae4426e81b966cb550cc0ee676b
MingAn <113867200+LMingAn@users.noreply.github.com>
2022-09-25 23:05:02 +0800

    Add files via upload

---
+++ OJ practice_9.25.cpp
@@ -0,0 +1,21 @@
+﻿//cjc03305
+#include<stdio.h>
+/*int main()
+{
+    int a;
+    scanf("%d", &a);
+    if (a % 13 == 0)
+        printf("yes");
+    else
+        printf("no");
+    return 0;
+}*/
+
+#include<stdio.h>
+int main()
+{
+    int a, b, c, d, e, f, g;
+    scanf_s("%d %d %d %d %d %d %d 0", &a, &b, &c, &d, &e, &f, &g);
+    printf("%d %d %d %d %d %d %d", g, f, e, d, c, b, a);
+    return 0;
+}
