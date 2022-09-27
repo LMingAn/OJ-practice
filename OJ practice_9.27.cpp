d1b28e6aac3e38186e438e4b2810771b999a558b
MingAn <113867200+LMingAn@users.noreply.github.com>
2022-09-27 23:54:18 +0800

    Add files via upload

---
+++ OJ practice_9.27.cpp
@@ -0,0 +1,27 @@
+﻿//KJZ-2101
+/*#include<stdio.h>
+int main()
+{
+	int A, B, C;
+	scanf("%d %d", &A, &B);
+	C = A + B;
+	printf("%d", C);
+	return 0;
+}*/
+
+//lq0901
+/*#include <stdio.h>
+int main()
+{
+	printf("125");
+
+	return 0;
+}*/
+
+#include<stdio.h>
+int main()
+{
+	scanf_s("1");
+	printf("1");
+	return 0;
+}
