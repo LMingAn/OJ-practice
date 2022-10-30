65deb034ce5a0634d1040965e33bfd839420a4fe
MingAn <113867200+LMingAn@users.noreply.github.com>
2022-10-30 10:24:09 +0800

    Add files via upload

--- KJZ-XXXX.cpp
+++ KJZ-XXXX.cpp
@@ -8,3 +8,21 @@ int main()
 	printf("%d", C);
 	return 0;
 }*/
+
+
+//KJZ-2103
+#include<stdio.h>
+int main()
+{
+	int n, a, b = 1, c = 1, d = 1;
+	scanf("%d", &n);
+	if (n > 2)
+		for (a = 3; a <= n; a++)
+		{
+			d = b + c;
+			b = c;
+			c = d;
+		}
+	printf("%d", d);
+	return 0;
+}
