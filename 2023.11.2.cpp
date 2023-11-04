ae73e519429eb41cfd2566586c1924cde6e039f9
MingAn <113867200+LMingAn@users.noreply.github.com>
2023-11-04 16:04:19 +0800

    Add files via upload

--- 2023.11.2.cpp
+++ 2023.11.2.cpp
@@ -1,31 +1,76 @@
 #include <iostream>
 #include <string>
-#include <unordered_map>
+#include <algorithm>
 using namespace std;
 
+/*
+第三题
+题目描述
+Given a word, please output its number of occurrences and first occurrence position in the given string.
 
+输入格式
+First line is a string containing only letters, representing a given word.
+
+Second line is a string that may only contain letters and spaces, representing the given string.
+
+输出格式
+One line, if a given word is found in the string, two integers are output, separated by a space, indicating the number of times the word appears in the string and the position of the first occurrence. If the word does not appear in the string, output -1 directly.
+
+输入输出样例
+输入 #1复制
+to
+to be or not to be is a question
+输出 #1复制
+2 0
+输入 #2复制
+to
+to be or not to be is a together question
+输出 #2复制
+2 0
+说明/提示
+读入字符串时：
+
+string s;
+
+然后使用
+
+getline(cin, s);
+
+替代
+
+cin >> s;
+
+这样可以保证读入空格。
+*/
 int main()
 {
-	unordered_map<string, long> word;
 	string s1, s2;
-	long n = 0, m = 0;
+	long n = 0, m = 0, nm = 0;//n为首次遍历，m为出现次数，nm为动态首次出现地点
 	cin >> s1;
-	for (n = 0, cin >> s2; s2.end()-1 != " "; ++n)
+	transform(s1.begin(), s1.end(), s1.begin(), ::tolower);//大小写转化 -- 转化为小写
+	getchar();
+	s1 += " ";
+	getline(cin, s2);
+	transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+	s2 += " ";
+	nm = n = s2.find(s1);//首次遍历查询
+	while (s2.size() != 0 && n != -1)//重复遍历查询
 	{
-		if (s1 == s2)
+		nm = s2.find(s1);
+		if (nm == -1)
+			break;
+		else
 		{
-			if (word[s1] == 0)
-			{
-				m = n;
-			}
-			word[s1]++;
+			++m;
+			s2.erase(0, nm + s1.size());
 		}
 	}
-	unordered_map<string, long>::iterator it;
-	cout << it->second << " " << m;
+	if (n == -1)
+		cout << n;
+	else
+		cout << m << " " << n;
 	return 0;
 }
-
 
 
 //int main()
