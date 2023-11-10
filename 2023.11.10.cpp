d7881af0706fe710503d5e041ef3817690cc9c99
MingAn <113867200+LMingAn@users.noreply.github.com>
2023-11-11 00:21:27 +0800

    Add files via upload

---
+++ 2023.11.10.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	cin >> n;
+
+}
+
+
+
+//int main()
+//{
+//	int wind = 0, sumq = 0, suml = 0, sumj = 0;
+//	for (int a = 0; a < 8; ++a)
+//	{
+//		cin >> wind;
+//		if (wind >= 118)
+//		{
+//			cout << 10;
+//			return 0;
+//		}
+//		if (wind >= 63)
+//			++suml;
+//		else if (wind >= 41)
+//			++sumq;
+//	}
+//	if (suml >= sumq && suml >= 4)
+//		cout << 8;
+//	else if (sumq >= 4)
+//		cout << 3;
+//	else
+//		cout << 1;
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int ea = 0, ha = 0, tea = 0, tha = 0, gra = 0, sumea = 0, sumha = 0, sum = 0;
+//	cin >> ea >> ha >> tea >> tha >> gra;
+//	sumea = tea * 5;
+//	sumha = tha * 20 - (ha - tha) * 20;
+//	if (sumea >= gra)
+//		sum = sumea + sumha;
+//	else
+//		sum = sumea - (ha - tha) * 20;
+//	if (sum > 0)
+//		cout << sum;
+//	else
+//		cout << 0;
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int ter = 0, stu = 0, R = 0, V = 0, M = 0;
+//	long nR = 0, nV = 0, nM = 0;
+//	cin >> ter >> stu >> R >> V >> M;
+//	nR = 1 * stu * R + 2 * ter * 2 * R;
+//	nV = 1 * stu * V + 2 * ter * 3 * V;
+//	nM = 1 * stu * M + 2 * ter * 3 * M;
+//	cout << nR << " " << nV << " " << nM;
+//	return 0;
+//}
